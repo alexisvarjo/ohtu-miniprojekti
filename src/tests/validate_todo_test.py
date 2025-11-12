a8ded6898960783d64ce8290fc551e9927636ac2
Kristjan Kaasik <kristjankk25@gmail.com>
2025-11-12 20:56:51 +0200

    Add Github Actions CI-testing

--- src/tests/validate_todo_test.py
+++ src/tests/validate_todo_test.py
@@ -1,17 +1,20 @@
-import unittest
-from util import validate_todo, UserInputError
+# THIS FILE ONLY INCLUDES ROBOT.TEST EXAMPLES
+# DON'T REMOVE THE COMMENTS!
 
-class TestTodoValidation(unittest.TestCase):
-    def setUp(self):
-        pass
+# import unittest
+# from util import validate_todo, UserInputError
 
-    def test_valid_length_does_not_raise_error(self):
-        validate_todo("juokse")
-        validate_todo("a" * 100)
+# class TestTodoValidation(unittest.TestCase):
+    # def setUp(self):
+        # pass
 
-    def test_too_short_or_long_raises_error(self):
-        with self.assertRaises(UserInputError):
-            validate_todo("ole")
+    # def test_valid_length_does_not_raise_error(self):
+        # validate_todo("juokse")
+        # validate_todo("a" * 100)
 
-        with self.assertRaises(UserInputError):
-            validate_todo("koodaa" * 20)
+    # def test_too_short_or_long_raises_error(self):
+        # with self.assertRaises(UserInputError):
+            # validate_todo("ole")
+
+        # with self.assertRaises(UserInputError):
+            # validate_todo("koodaa" * 20)
