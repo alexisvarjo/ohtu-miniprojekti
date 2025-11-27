83913b18d4c65eb0998a006accca7dac4cd51885
Kristjan Kaasik <kristjankk25@gmail.com>
2025-11-27 21:44:18 +0200

    Refactor the DOI crawler

--- src/doi_crawler.py
+++ src/doi_crawler.py
@@ -26,13 +26,8 @@ INPROCEEDING_TYPES = [
     "conference-paper"
 ]
 
-
-def citation_with_doi(doi, citekey, tag):
-    """Fetches citation information with a DOI"""
-
-    if check_if_citekey_exists(citekey):
-        return "The citekey already exists"
-
+def _fetch_doi_citation(doi):
+    """Fetches citation information with a DOI and turns the information into a JSON"""
     try:
         response = requests.get(
             f"https://doi.org/{doi}",
@@ -40,109 +35,134 @@ def citation_with_doi(doi, citekey, tag):
             timeout=10,
         )
     except:
-        return "The URL doesn't exist"
-
+        return ("The URL doesn't exist", None)
+    
     if response.status_code == 404:
-        return "The requested DOI doesn't exist"
-
+        return ("The requested DOI doesn't exist", None)
+    
     try:
         data = response.json()
     except:
-        return "Error"
+        return ("Error", None)
     
+    return ("success", data)
+
+def _parse_add_article(data, citekey, tag):
+    """Parses an article DOI"""
+    authors = []
+    for author in data["author"]:
+        given = author.get("given", "")
+        family = author.get("family", "")
+        authors.append(f"{given} {family}")
+    author = ", ".join(authors)
+
+    year = data["issued"]["date-parts"][0][0]
+    name = data.get("title", "")
+    journal = data.get("container-title", "")
+    volume = data.get("volume", "")
+    number = data.get("issue", "")
+    urldate = ""
+    url = data.get("URL", "")
+
+    create_article(
+        citekey, author, year, name, journal, volume, number, urldate, url, tag
+    )
+
+    return
+
+def _parse_add_book(data, citekey, tag):
+    """Parses a book DOI"""
+    authors = []
+    for author in data["author"]:
+        given = author.get("given", "")
+        family = author.get("family", "")
+        authors.append(f"{given} {family}")
+    author = ", ".join(authors)
+
+    try:
+        editors = []
+        for editor in data["editor"]:
+            given = editor.get("given", "")
+            family = editor.get("family", "")
+            editors.append(f"{given} {family}")
+        editor = ", ".join(editors)
+    except:
+        editor = None
+
+    title = data.get("title", "")
+    publisher = data.get("publisher", "")
+    year = data["issued"]["date-parts"][0][0]
+    volume = data.get("volume", "")
+    number = data.get("issue", "")
+    urldate = ""
+    url = data.get("URL", "")
+
+    create_book(
+        citekey, author, editor, title, publisher, year, volume, number, urldate, url, tag
+    )
+
+    return
+
+def _parse_add_inproceeding(data, citekey, tag):
+    """Parses an inproceeding DOI"""
+    authors = []
+    for author in data["author"]:
+        given = author.get("given", "")
+        family = author.get("family", "")
+        authors.append(f"{given} {family}")
+    author = ", ".join(authors)
+
+    try:
+        editors = []
+        for editor in data["editor"]:
+            given = editor.get("given", "")
+            family = editor.get("family", "")
+            editors.append(f"{given} {family}")
+        editor = ", ".join(editors)
+    except:
+        editor = None
+
+    title = data.get("title", "")
+    booktitle = data.get("container-title", "")
+    publisher = data.get("publisher", "")
+    pages = data.get("page", "")
+    year = data["issued"]["date-parts"][0][0]
+    volume = data.get("volume", "")
+    number = data.get("issue", "")
+    urldate = ""
+    url = data.get("URL", "")
+
+    create_inproceeding(
+        citekey, author, editor, title, booktitle, publisher, pages, year, volume, number, urldate, url, tag
+    )
+
+    return
+
+def citation_with_doi(doi, citekey, tag):
+    """Adds a citation with a doi"""
+    if check_if_citekey_exists(citekey):
+        return "The citekey already exists"
+
+    data = _fetch_doi_citation(doi)
     print(data)
+    if data[0] != "success":
+        return data[0] # error message
+    else:
+        data = data[1] # data
 
-    # articles
     if data["type"] in ARTICLE_TYPES:
-        authors = []
-        for author in data["author"]:
-            given = author.get("given", "")
-            family = author.get("family", "")
-            authors.append(f"{given} {family}")
-        author = ", ".join(authors)
-
-        year = data["issued"]["date-parts"][0][0]
-        name = data.get("title", "")
-        journal = data.get("container-title", "")
-        volume = data.get("volume", "")
-        number = data.get("issue", "")
-        urldate = ""
-        url = data.get("URL", "")
-
-        create_article(
-            citekey, author, year, name, journal, volume, number, urldate, url, tag
-        )
-
-    # books
+        _parse_add_article(data, citekey, tag)
     elif data["type"] in BOOK_TYPES:
-        authors = []
-        for author in data["author"]:
-            given = author.get("given", "")
-            family = author.get("family", "")
-            authors.append(f"{given} {family}")
-        author = ", ".join(authors)
-
-        try:
-            editors = []
-            for editor in data["editor"]:
-                given = editor.get("given", "")
-                family = editor.get("family", "")
-                editors.append(f"{given} {family}")
-            editor = ", ".join(editors)
-        except:
-            editor = None
-
-        title = data.get("title", "")
-        publisher = data.get("publisher", "")
-        year = data["issued"]["date-parts"][0][0]
-        volume = data.get("volume", "")
-        number = data.get("issue", "")
-        urldate = ""
-        url = data.get("URL", "")
-
-        create_book(
-            citekey, author, editor, title, publisher, year, volume, number, urldate, url, tag
-        )
-
-    # inproceedings
+        _parse_add_book(data, citekey, tag)
     elif data["type"] in INPROCEEDING_TYPES:
-        authors = []
-        for author in data["author"]:
-            given = author.get("given", "")
-            family = author.get("family", "")
-            authors.append(f"{given} {family}")
-        author = ", ".join(authors)
-
-        try:
-            editors = []
-            for editor in data["editor"]:
-                given = editor.get("given", "")
-                family = editor.get("family", "")
-                editors.append(f"{given} {family}")
-            editor = ", ".join(editors)
-        except:
-            editor = None
-
-        title = data.get("title", "")
-        booktitle = data.get("container-title", "")
-        publisher = data.get("publisher", "")
-        pages = data.get("page", "")
-        year = data["issued"]["date-parts"][0][0]
-        volume = data.get("volume", "")
-        number = data.get("issue", "")
-        urldate = ""
-        url = data.get("URL", "")
-
-        create_inproceeding(
-            citekey, author, editor, title, booktitle, publisher, pages, year, volume, number, urldate, url, tag
-
-        )
+        _parse_add_inproceeding(data, citekey, tag)
     else:
         return "The citation type is not supported"
-
 
     return "success"
 
 
 if __name__ == "__main__":
-    citation_with_doi("10.1038/nature11631", "testing")
+    citation_with_doi("10.1038/nature11631", "article", "article_tag") # article
+    citation_with_doi("10.1007/978-3-031-45468-4", "book", "book_tag") # book
+    citation_with_doi("10.1109/CVPR.2016.90", "inproceedings", "inproceedings_tag") # inproceeding
